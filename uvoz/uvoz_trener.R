4ef391f84597d88b21be364d1e44ab102a98140f
Samo Metličar <samo.metlicar@hotmail.com>
2016-04-28 18:53:14 +0200

    Trenerji
    Dodal manjkajoče trenerje

--- uvoz/uvoz_trener.R
+++ uvoz/uvoz_trener.R
@@ -41,3 +41,38 @@ trenerji$Name <- ime_p
 #   igralci[igralci$Name == i, 5] <- trenerji[trenerji$Coaches == i, 1]
 # }
 # colnames(igralci)[5] <- 'Coach'
+
+#Dodajmo sedaj manjkajoče trenerje in pripadajoče igralce (NI ZA VSE NAPISANO, DA IMAJO 2 TRENERJA)
+rownames(trenerji) <- NULL
+trenerji[length(trenerji$Name)+1, ] <- c('Boris Becker', 'Novak Djokovic')
+trenerji[length(trenerji$Name)+1, ] <- c('Severin Luthi', 'Roger Federer')
+trenerji[length(trenerji$Name)+1, ] <- c('Magnus Norman', 'Stanislas Wawrinka')
+trenerji[length(trenerji$Name)+1, ] <- c('Thierry Ascione', 'Jo-Wilfried Tsonga')
+trenerji[length(trenerji$Name)+1, ] <- c('Francisco Fogues', 'David Ferrer')
+trenerji[length(trenerji$Name)+1, ] <- c('Segi Bruguera', 'Richard Gasquet')
+trenerji[length(trenerji$Name)+1, ] <- c('Riccardo Piatti', 'Milos Raonic')
+trenerji[length(trenerji$Name)+1, ] <- c('Goran Ivanisevic', 'Marin Cilic')
+trenerji[length(trenerji$Name)+1, ] <- c('Thierry Van Cleemput', 'David Goffin')
+trenerji[length(trenerji$Name)+1, ] <- c('Mikael Tillstrom', 'Gael Monfils')
+trenerji[length(trenerji$Name)+1, ] <- c('Justin Gimelstob', 'John Isner')
+trenerji[length(trenerji$Name)+1, ] <- c('Pepe Vendrell', 'Roberto Bautista Agut')
+trenerji[length(trenerji$Name)+1, ] <- c('Lleyton Hewitt', 'Nick Kyrgios') ###### PREVERI, ČE JE RES
+trenerji[length(trenerji$Name)+1, ] <- c('Lionel Zimbler', 'Benoit Paire') 
+trenerji[length(trenerji$Name)+1, ] <- c('John Tomic', 'Bernard Tomic')
+trenerji[length(trenerji$Name)+1, ] <- c('Jack Reader', 'Viktor Troicki')
+trenerji[length(trenerji$Name)+1, ] <- c('Troy Hahn', 'Jack Sock')
+trenerji[length(trenerji$Name)+1, ] <- c('Craig Boynton', 'Steve Johnson')
+trenerji[length(trenerji$Name)+1, ] <- c('Magnus Tideman', 'Jeremy Chardy')
+trenerji[length(trenerji$Name)+1, ] <- c('João Zwestch', 'Thomaz Bellucci') ###### POSEBEN 'a' -> ã
+trenerji[length(trenerji$Name)+1, ] <- c('Craig Boynton', 'Sam Querrey') ###### ISTI TRENER KOT PRI 'Steve Johnson'
+trenerji[length(trenerji$Name)+1, ] <- c('Dejan Vojnovic', 'Marcos Baghdatis')
+trenerji[length(trenerji$Name)+1, ] <- c('Alexander Kuznetsov', 'Andrey Kuznetsov')
+trenerji[length(trenerji$Name)+1, ] <- c('Massimo Sartori', 'Andreas Seppi')
+trenerji[length(trenerji$Name)+1, ] <- c('Alexandre Lisiecki', 'Gilles Muller')
+trenerji[length(trenerji$Name)+1, ] <- c('Gustavo Marcaccio', 'Guido Pella')
+trenerji[length(trenerji$Name)+1, ] <- c('Martin Hromec', 'Martin Klizan')
+trenerji[length(trenerji$Name)+1, ] <- c('Thierry Ascione', 'Nicolas Mahut') ###### IMA 2 TRENERJA + VPISAN TRENER SE PONOVI ŠE PRI 'Jo-Wilfried Tsonga'
+trenerji[length(trenerji$Name)+1, ] <- c('Alexander Zverev Sr.', 'Alexander Zverev')
+trenerji[length(trenerji$Name)+1, ] <- c('Samuel Lopez', 'Pablo Carreno Busta') ###### IMA 2 TRENERJA
+
+
