a9c10df5c0c905a84e105a064a8129dc48ab0453
Neza Ahcin <neza.ahcin@gmail.com>
2016-05-29 20:05:45 +0200

    urejanje imena turnirjev

---
+++ urejanje/urejanje_head2head.R
@@ -0,0 +1,11 @@
+library(stringr)
+
+#uredimo imena turinrjev
+head2head$Tournament <- sub('\\ 1.*', '', head2head$Tournament)
+
+head2head$Tournament[head2head$Tournament == 'Roland Garros'] <- 'French Open'
+head2head$Tournament[head2head$Tournament == 'Rome Masters'] <- 'Italian Open'
+head2head$Tournament[head2head$Tournament == 'Madrid Masters'] <-'Madrid Open'
+head2head$Tournament[head2head$Tournament == 'Miami Masters'] <-'Miami Open'
+head2head$Tournament[head2head$Tournament == 'Montreal Masters'] <-'Canadian Open'
+head2head$Tournament[head2head$Tournament == 'Toronto Masters'] <-'Canadian Open'
