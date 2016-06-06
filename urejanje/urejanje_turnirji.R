7e53a6dda42dfdfb9b84583809fc3f95fef00348
Samo Metličar <samo.metlicar@hotmail.com>
2016-06-06 15:25:55 +0200

    Popravljeni in dodani manjkajoči turnirji

--- urejanje/urejanje_turnirji.R
+++ urejanje/urejanje_turnirji.R
@@ -19,3 +19,38 @@ turnirji$City[turnirji$ID == 'Canadian Open' & turnirji$Year %% 2 == 1] <- 'Mont
 turnirji$City[turnirji$ID == 'Canadian Open' & turnirji$Year %% 2 == 0] <- 'Toronto'
 
 turnirji$City[turnirji$ID == 'ATP WT Finals'] <- 'London'
+
+manjkajoci <- c()
+for(i in 1:3759){
+  stevec <- 0
+  for(j in 1:84){
+    if((head2head$Tournament[i] == turnirji$ID[j]) & (head2head$Year[i] == turnirji$Year[j])){
+      stevec <- stevec + 1
+      break
+    }
+  }
+  if(stevec == 0){
+    manjkajoci <- c(manjkajoci, i)
+  }
+}
+
+turnir <- c('Wimbledon')
+leto <- c(2010)
+for(i in manjkajoci){
+  stevec <- 0
+  for(j in 1:length(turnir)){
+    if((head2head$Tournament[i] == turnir[j]) & (head2head$Year[i] == leto[j])){
+      stevec <- stevec + 1
+      break
+    }
+  }
+  if(stevec == 0){
+    turnir <- c(turnir, head2head$Tournament[i])
+    leto <- c(leto, head2head$Year[i])
+  }
+}
+
+dodatni_turnirji <- data.frame(ID=turnir, Year=leto, NA, NA, NA, NA, NA, NA)
+turnirji <- as.data.frame(rbind(as.matrix(turnirji), as.matrix(dodatni_turnirji)))
+
+
