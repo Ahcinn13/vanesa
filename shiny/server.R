c68da249b8c17a03e8b64a970d3992ef5dfe6788
Neza Ahcin <neza.ahcin@gmail.com>
2016-06-07 17:49:44 +0200

    shiny

---
+++ shiny/server.R
@@ -0,0 +1,45 @@
+library(shiny)
+library(dplyr)
+library(RPostgreSQL)
+library(ggplot2)
+library(DT)
+library(rworldmap)
+library(maps)
+library(sp)
+library(leaflet)
+library(gsubfn)
+library(RColorBrewer)
+
+
+if ("server.R" %in% dir()) {
+  setwd("..")
+}
+#source("4.Baza/auth.R")
+source("auth_public.R")
+
+#########################################################
+
+shinyServer(function(input, output) {
+  #  Vzpostavimo povezavo
+  conn <- src_postgres(dbname = db, host = host,
+                       user = user, password = password)
+  #  Pripravimo tabelo
+
+  tbl.player <- tbl(conn, "player")
+  tbl.statistics <- tbl(conn, "statistics")
+
+  
+ 
+  output$sta <- DT::renderDataTable({
+    # Naredimo poizvedbo
+    t <- tbl.statistics %>%  filter(name == input$tenisaci) %>% data.frame()
+    
+  })
+  
+  output$tenisaci <- renderUI({
+    igralci <- data.frame(tbl.player)
+    selectInput("tenisaci", "Choose a player:",
+                choices = c("All" = 0, setNames(igralci$name,
+                                                igralci$name)))
+  })
+})
